228faf1a801cb8b0f3c006662bed472b357a5854
Kurawruwu <valentinherreman7@gmail.com>
2023-03-07 22:00:06 +0100

    ajout plateforme mouvante

---
+++ Assets/scripts/MovingPlatform.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+    private Transform target;
+    private int destPoint=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}

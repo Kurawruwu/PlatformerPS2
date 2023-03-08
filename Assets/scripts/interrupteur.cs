ab6c8cd652828aa4451b6340b323982ced32b954
Kurawruwu <valentinherreman7@gmail.com>
2023-03-08 10:25:18 +0100

    update interrupteur

--- Assets/scripts/interrupteur.cs
+++ Assets/scripts/interrupteur.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class interrupteur : MonoBehaviour
 {
     [SerializeField] bool isOn = false;
+    bool inZone = false;
 
 
     // Start is called before the first frame update
@@ -16,20 +17,27 @@ public class interrupteur : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.F) && inZone)
+        {
+            isOn = !isOn;
+            Debug.Log(isOn);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("dans la zone");
-
         if (collision.transform.name == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                isOn = true;
-                Debug.Log("appuie");
-            }
+            //Debug.Log("dans la zone");
+            inZone = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            //Debug.Log("dehors la zone");
+            inZone = false;
         }
     }
 }

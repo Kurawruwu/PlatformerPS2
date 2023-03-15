f45024155cff57e820d61281e4b91c1e95c8a4fe
Kurawruwu <valentinherreman7@gmail.com>
2023-03-15 13:46:46 +0100

    update plateforme qui tombe

--- Assets/scripts/FallingPlatform.cs
+++ Assets/scripts/FallingPlatform.cs
@@ -6,9 +6,18 @@ public class FallingPlatform : MonoBehaviour
 {
     float fallDelay = 1f;
     float destroyDelay = 2f;
-
+    float spawnDelay = 3f;
+    SpriteRenderer sr;
+    BoxCollider2D box;
     [SerializeField] Rigidbody2D rb;
+    Vector2 spawnPosition;
+    private void Start()
+    {
 
+       spawnPosition = gameObject.transform.position;
+        sr = GetComponent<SpriteRenderer>();
+        box = GetComponent<BoxCollider2D>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
@@ -21,7 +30,15 @@ public class FallingPlatform : MonoBehaviour
     {
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, destroyDelay);
+        yield return new WaitForSeconds(destroyDelay);
+        sr.enabled = false;
+        box.enabled = false;
+        yield return new WaitForSeconds(spawnDelay);
+        sr.enabled = true;
+        box.enabled = true;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = Vector2.zero;
+        gameObject.transform.position = spawnPosition;
     }
 
 }

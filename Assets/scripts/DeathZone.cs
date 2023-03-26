ca50be06a6cb79e80086e215a3fd17d0eec81644
Kurawruwu <valentinherreman7@gmail.com>
2023-03-26 23:00:53 +0200

    update checkpoint et ajout de zones d elimination

--- Assets/scripts/DeathZone.cs
+++ Assets/scripts/DeathZone.cs
@@ -1,21 +1,17 @@
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class DeathZone : MonoBehaviour
 {
-    private Transform playerSpawn;
-
+    Transform playerSpawn;
     private void Awake()
     {
         playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            playerSpawn.position = transform.position;
-            Destroy(gameObject);
+            collision.transform.position = playerSpawn.position;
         }
     }
-
 }
